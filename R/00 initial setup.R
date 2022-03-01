2c8f3eee929be4eb248b7ad86ea101f7a39cbcd6
Josh <joshua.wallace@cabinetoffice.gov.uk>
2022-03-01 14:25:36 +0000

    Manual action setup

---
+++ R/00 initial setup.R
@@ -0,0 +1,10 @@
+# library(tidyverse)
+# library(magrittr)
+# library(readxl)
+# library(unpivotr)
+# library(tidyxl)
+#
+# ################################################################################
+# source("./R/setup functions/template_conversion.R")
+# # Converts clean versions of the data templates into .rds
+# # to be used when checking if downloaded files match the expected format
