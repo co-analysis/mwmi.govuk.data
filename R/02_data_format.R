0fa3c0eb0ef3f08761e0199264c839729dce9129
Josh <joshua.wallace@cabinetoffice.gov.uk>
2022-03-11 20:01:32 +0000

    start testing data formatting

--- R/02_data_format.R
+++ R/02_data_format.R
@@ -1,35 +1,48 @@
+library(mwmi.govuk.scraper)
 # library(tidyverse)
 # library(magrittr)
 # library(readxl)
 # library(unpivotr)
 # library(tidyxl)
 #
-# ################################################################################
-# source("./R/format functions/template filtering.R")
-#
-# # Get list of rds files to check
-# gov_rds <- list.files("./data/gov_files_rds",
-#                       pattern=".rds$",
-#                       include.dirs=FALSE,
-#                       full.names=TRUE,
-#                       recursive=TRUE)
-# # Add in a blank template to format for use later
-# gov_rds <- c(gov_rds,"data/templates/import/blank accessible template.xlsx.rds")
-#
-# # TODO: filter out those which have already been checked
-#
-# # Check all files to see if they match against the prescribed template
-# match_format_results <- map(gov_rds,find_matching_sheets)
-#
-# which_matching <- which(map(match_format_results,~ .x$any_sheets) %>% unlist)
-#
-# # Output results for QA
+###############################################################################
+# Get list of rds files to check
+gov_rds <- list.files("data/gov_files_rds",
+                      pattern=".rds$",
+                      include.dirs=FALSE,
+                      full.names=TRUE,
+                      recursive=TRUE)
+# Add in a blank template to format for use later
+gov_rds <- c(gov_rds,"data/templates/import/blank accessible template.xlsx.rds")
+
+# TODO: filter out those which have already been checked
+
+# Read templates into a dataset to merge on
+template_format_files <- list.files("data/templates/rds",
+                                    pattern="^[^~]",
+                                    include.dirs=FALSE,
+                                    full.names=TRUE,
+                                    recursive=TRUE)
+template_formats <- map(template_format_files,~ readRDS(.x)[[1]] %>%
+                          filter(!is.na(chr)) %>%
+                          mutate(template_chr=text_sanitiser(chr)) %>%
+                          select(address,row,col,template_chr) %>%
+                          mutate(template=gsub(".*?\\/([^\\/]+)\\.rds","\\1",.x))) %>%
+  bind_rows
+# TODO: currently being set and read from environment - better way?
+
+# Check all files to see if they match against the prescribed template
+match_format_results <- map(gov_rds,find_matching_sheets)
+
+which_matching <- which(map(match_format_results,~ .x$any_sheets) %>% unlist)
+
+# Output results for QA
 # write_rds(match_format_results,paste0("data/gov_meta/match_format_results.rds"))
 # write_rds(which_matching,paste0("data/gov_meta/which_matching.rds"))
-#
-# # Load matching datasets
-# rds_data <- map(gov_rds[which_matching],readRDS)
-#
+
+# Load matching datasets
+rds_data <- map(gov_rds[which_matching],readRDS)
+
 # ################################################################################
 # # Extract matching tabs
 # matching_data <- map2(rds_data,match_format_results[which_matching], ~ if (.y$any_sheets==TRUE) .x[.y$which_sheets] else NULL) %>%
